1ff1f22550192233b3258087dfdb42051c9a9bcd
youdonghee <0103donghee@naver.com>
2023-06-26 13:29:52 +0900

    2

--- backEnd/controllers/joinControllers.js
+++ backEnd/controllers/joinControllers.js
@@ -8,6 +8,8 @@ exports.joinUser = async(req,res)=>{
     const { user_id, user_pw, username } = req.body.data;
     const user = await User.findOne({where : {user_id}})
     const usernamecheck = await User.findOne({where : {username}});
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+    
     if (user !== null) {
       console.log(user_id,"중복된 아이디");
       return res.json({message:"중복된 아이디 입니다"});
@@ -19,7 +21,6 @@ exports.joinUser = async(req,res)=>{
     }
     
     // 정규식
-    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
     if (!passwordRegex.test(user_pw)) {
       console.log("비밀번호 정규식 아님");
       return res.json({message:"비밀번호 정규식 아님"})
